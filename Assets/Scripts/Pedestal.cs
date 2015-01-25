e1f84c78315538fe57d8951073f13e39716ae6cd
ZachLHelms <ZachLHelms@gmail.com>
2015-01-25 13:51:21 -0500

    Added a class that manages the reveal of various objects in the scene

---
+++ Assets/Scripts/Pedestal.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pedestal : MonoBehaviour {
+
+	[SerializeField]
+	private Reveal[] earthGemReveals;
+	[SerializeField]
+	private Reveal[] waterGemReveals;
+	[SerializeField]
+	private Reveal[] fireGemReveals;
+	[SerializeField]
+	private Reveal[] windGemReveals;
+
+	public void RunReveal(int revealID) {
+		Reveal[] toReveal;
+		switch (revealID) {
+		case 0:
+			toReveal = earthGemReveals;
+			break;
+		case 1:
+			toReveal = waterGemReveals;
+			break;
+		case 2:
+			toReveal = fireGemReveals;
+			break;
+		case 3:
+			toReveal = windGemReveals;
+			break;
+		default:
+			Debug.LogError("Reveal ID not a proper id.");
+			return;
+		}
+		for (int i = 0; i < toReveal.Length; ++i) {
+			toReveal[i].Play();
+		}
+	}
+}
