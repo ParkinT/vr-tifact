893c81929fec681f830211f801eb7117ffa1fa47
Matt Hansen <m.hansen@knights.ucf.edu>
2015-01-25 11:55:57 -0500

    Added world scene with gesture control

---
+++ Assets/Scripts/Contact.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Contact : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(string.Format("Yo, {0}", other.gameObject.name));
+    }
+}
