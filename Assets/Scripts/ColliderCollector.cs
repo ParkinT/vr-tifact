116f97f362e67d499c2f0386a8c25314d09f3c47
Zach Helms <ZachLHelms@gmail.com>
2015-01-28 02:14:20 -0500

    ColliderCollector.cs, created an object that maintains a list of trigger colliders that are currently colliding with it

---
+++ Assets/Scripts/ColliderCollector.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ColliderCollector : MonoBehaviour {
+	
+	public List<Collider> managedColliders = new List<Collider>();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		managedColliders.Add (other);
+	}
+	
+	void OnTriggerExit(Collider other)
+	{
+		if (managedColliders.Contains (other)) {
+			managedColliders.Remove(other);
+		}
+	}
+}
