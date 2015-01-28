c0a02d97429f7d88ed289125f04c7e1e78a5aa67
Zach Helms <ZachLHelms@gmail.com>
2015-01-28 02:16:47 -0500

    created a MouseCarrier for managing mouse-based input carrying

---
+++ Assets/Scripts/MouseCarrier.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseCarrier : Carrier {
+	
+	private bool bLMBState = false, bPrevLMBState = false;
+
+	private ColliderCollector colliderCollector = null;
+	
+	// Use this for initialization
+	void Start () {
+		this.colliderCollector = this.GetComponent<ColliderCollector>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		base.Update ();
+
+		bPrevLMBState = bLMBState;
+		bLMBState = Input.GetButton ("Fire1");
+		
+		if (bLMBState != bPrevLMBState) {
+			// Drop gesture
+			if ((!bLMBState && bPrevLMBState) && base.isHoldingObj)
+			{
+				print("Drop object");
+				base.DropObject();
+			}
+			
+			// Pick up gesture
+			if (((bLMBState && !bPrevLMBState)) && !base.isHoldingObj)
+			{
+				GameObject pickup = null;
+				foreach(Collider c in this.colliderCollector.managedColliders) {
+					if(c.tag.Equals("ball")) { 
+						pickup = c.gameObject;
+						break;
+					}
+					else if (c.tag.Equals("gem")) {
+						pickup = c.gameObject;
+					}
+				}
+				if(null != pickup) {
+					Carryable carryable = pickup.GetComponent<Carryable>();
+					if(null != carryable){
+						Debug.Log("Pickup object");
+						base.PickupObject(carryable);
+					}
+				}
+			}
+		}
+	}
+
+	public override void DropObject ()
+	{
+		Collider collider = base._heldObject.collider;
+		if (this.colliderCollector.managedColliders.Contains (collider)) {
+			this.colliderCollector.managedColliders.Remove (collider);
+		}
+		base.DropObject ();
+	}
+}
